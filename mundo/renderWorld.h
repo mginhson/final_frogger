92852a182c46ecd5463ea5f75392a05ba920d9a1
SWainer <swainer@itba.edu.ar>
2024-07-08 21:37:24 -0300

    actualizo render

--- mundo/renderWorld.h
+++ mundo/renderWorld.h
@@ -4,7 +4,7 @@
 #include "../entities/entities.h"
 #include "../config.h"
 
-int renderWorld (map_t * map, const independent_object_t * frog[],int size ,int tiempo, int vidasRestantes);
+int renderWorld (map_t * map, const independent_object_t * frog[],int size ,int tiempo);
 extern int contador;
 
 enum contadorTiempo {noChanges, nextStep, restart};
